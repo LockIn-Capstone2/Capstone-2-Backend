0f5253d46c33b41bb67d3b386a2be8f5f83146a1
KidCharles <sircharlyrivera@gmail.com>
2025-08-03 13:46:06 -0400

    cleaning up calculator model

--- database/Calculator.js
+++ database/Calculator.js
@@ -5,14 +5,25 @@ const Calculator = db.define('Calculator', {
   id: { 
     type: DataTypes.INTEGER, 
     primaryKey: true, 
-    autoIncrement: true },
-  assessment: DataTypes.TEXT,
-  grade: DataTypes.INTEGER,
-  weight: DataTypes.INTEGER,
+    autoIncrement: true,
+  },
   user_id: { 
     type: DataTypes.INTEGER, 
-    allowNull: false }
-
+    allowNull: false,
+  },
+  assessment: DataTypes.TEXT,
+  assignment_type: {
+    type: DataTypes.ENUM('Homework', 'Quiz', 'Midterm', 'Final'),
+    allowNull: false,
+  },
+  assignment_grades: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  assignment_weight: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
 module.exports = Calculator;
